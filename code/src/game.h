5d2a7392908cb888d3697ab5025806622777e36d
root <root@localhost.localdomain>
2017-03-21 02:32:09 -0400

    init directory

---
+++ code/src/game.h
@@ -0,0 +1,32 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include <string>
+
+#define BOARD_SIZE 15+1
+
+struct Position
+{
+    int x; 
+    int y;
+};
+
+class Game
+{
+public:
+    Game();
+    ~Game();
+    int Init();
+    int Run();
+
+private:
+    void PrintChessBoard();
+    int DownChessPiece(const char piece, const Position& pos);
+    int JudgeResult(const Position& pos);
+
+private:
+    std::string player1_;
+    std::string player2_;
+    char chess_board_[BOARD_SIZE][BOARD_SIZE];
+};
+#endif // _GAME_H_
